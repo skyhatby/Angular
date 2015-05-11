7e970d29becb3084c4af488f09a07906bd637db2
Skyhat <skyhatby@gmail.com>
2015-05-11 19:41:18 +0300

    Added VIew Models

---
+++ WebApp/Models/CriterionViewModel.cs
@@ -0,0 +1,48 @@
+﻿namespace WebApp.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CriterionViewModel
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the rate.
+        /// </summary>
+        /// <value>
+        /// The rate.
+        /// </value>
+        public string Rate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value.
+        /// </summary>
+        /// <value>
+        /// The value.
+        /// </value>
+        public string Value { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value rate.
+        /// </summary>
+        /// <value>
+        /// The value rate.
+        /// </value>
+        public string ValueRate { get; set; }
+    }
+}
