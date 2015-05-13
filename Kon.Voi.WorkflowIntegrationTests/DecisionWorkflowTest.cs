571284a5fcc49e99e4a1cfa177de853d7d54986b
skyhatby <skyhatby@gmail.com>
2015-05-13 15:08:15 +0300

    Fixed test

--- Kon.Voi.WorkflowIntegrationTests/DecisionWorkflowTest.cs
+++ Kon.Voi.WorkflowIntegrationTests/DecisionWorkflowTest.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
+using Kon.Voi.Model.DecisionModels;
 using Kon.Voi.Model.DecisionModels.ViewModels;
 using Kon.Voi.Workflow.Decision;
 using Microsoft.Practices.Unity;
@@ -31,6 +34,81 @@ namespace Kon.Voi.WorkflowIntegrationTests
         public UnityDependencyResolver DependencyResolver { get; set; }
 
         /// <summary>
+        /// Gets the test data.
+        /// </summary>
+        /// <returns></returns>
+        public DecisionViewModel GetTestData()
+        {
+            return new DecisionViewModel
+            {
+                Id = "0",
+                SessionName = "testData",
+                DecisionArray = new DecisionSubjectViewModel[]{
+                    new DecisionSubjectViewModel  {
+                        Id= "0",
+                        Name = "iPhone",
+                        CriteriaArray = new CriterionViewModel[]{
+                            new CriterionViewModel{
+                                Id =  "0",
+                                Name =  "design",
+                                Rate =  1,
+                                Value =  "brand",
+                                ValueRate =  1
+                            },
+                            new CriterionViewModel{
+                                Id =  "1",
+                                Name =  "price",
+                                Rate =  1,
+                                Value =  "1000",
+                                ValueRate =  1
+                            }
+                        }
+                    },
+                    new DecisionSubjectViewModel {
+                        Id =  "1",
+                        Name =  "Nokia Lumia 930",
+                        CriteriaArray = new CriterionViewModel[] {
+                            new CriterionViewModel{
+                                Id =  "0",
+                                Name =  "design",
+                                Rate =  1,
+                                Value =  "brand",
+                                ValueRate =  1
+                            },
+                            new CriterionViewModel{
+                                Id =  "1",
+                                Name =  "price",
+                                Rate =  1,
+                                Value =  "400",
+                                ValueRate =  1
+                            }
+                        }
+                    },
+                    new DecisionSubjectViewModel {
+                        Id =  "2",
+                        Name =  "Samsung Galaxy S5",
+                        CriteriaArray =  new CriterionViewModel[] {
+                            new CriterionViewModel{
+                                Id = "0",
+                                Name =  "design",
+                                Rate =  1,
+                                Value =  "brand",
+                                ValueRate =  1
+                            },
+                            new CriterionViewModel{
+                                Id =  "1",
+                                Name =  "price",
+                                Rate =  1,
+                                Value =  "700",
+                                ValueRate =  1
+                            }
+                        }
+                    }
+                }
+            };
+        }
+
+        /// <summary>
         /// Gets or sets the decision workflow.
         /// </summary>
         /// <value>
@@ -41,16 +119,19 @@ namespace Kon.Voi.WorkflowIntegrationTests
         /// <summary>
         /// Decisions the workflow count decision when passed normal data than returns correct data.
         /// </summary>
+        /// <exception cref="ArgumentNullException"><paramref /> or <paramref /> is null.</exception>
         [TestMethod]
         public void DecisionWorkflowCountDecision_PassedNormalData_returnsCorrectData()
         {
             AutoMapperConfiguration.Configure();
-            this.DecisionWorkflow = DependencyResolver.GetService<IDecisionWorkflow>();
-            DecisionViewModel decisionViewModel = new DecisionViewModel();
+            this.DecisionWorkflow = this.DependencyResolver.GetService<IDecisionWorkflow>();
+            DecisionViewModel workingDecision = this.GetTestData();
 
-            this.DecisionWorkflow.CountDecision(decisionViewModel);
-            //var c = UnityContainer.
+            DecisionViewModel result = this.DecisionWorkflow.CountDecision(workingDecision);
 
+            IEnumerable<bool> assertResult = result.DecisionArray.Select(x => Math.Abs(x.FinalRate - 33) < 1);
+            Assert.IsNotNull(result.DecisionArray.Any(x => Math.Abs(x.FinalRate - 33) < 1));
+            Assert.IsTrue(assertResult.Count() == 3);
         }
     }
 }
